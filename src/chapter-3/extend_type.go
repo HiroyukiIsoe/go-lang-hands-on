3df96629cc117afb4adb3d05de85f6f28f85c724
HiroyukiIsoe <hiroyuki.isoe@toranoana.co.jp>
2022-06-08 22:49:27 +0900

    add extend type src

---
+++ src/chapter-3/extend_type.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+type intp int
+
+func (num intp) isPrime() bool {
+	n := int(num)
+	for i := 2; i < (n/2); i++ {
+		if n%i == 0 {
+			return false
+		}
+	}
+	return true
+}
+
+func (num intp) PrimeFactor() []int {
+	ar := []int{}
+	x := int(num)
+	n := 2
+
+	for x > n {
+		if x%n == 0 {
+			x /= n
+			ar = append(ar, n)
+		} else {
+			if n == 2 {
+				n++
+			} else {
+				n += 2
+			}
+		}
+	}
+	ar = append(ar, x)
+	return ar
+}
+
+func (num *intp) doPrime() {
+	pf := num.PrimeFactor()
+	*num = intp(pf[len(pf)-1])
+}
+
+func main()  {
+	s := input("type a number")
+	n, _ := strconv.Atoi(s)
+	x := intp(n)
+	fmt.Printf("%d [%t].\n", x, x.isPrime())
+	fmt.Println(x.PrimeFactor())
+	x.doPrime()
+	fmt.Printf("%d [%t].\n", x, x.isPrime())
+	fmt.Println(x.PrimeFactor())
+	x++
+
+	fmt.Printf("%d [%t].\n", x, x.isPrime())
+	fmt.Println(x.PrimeFactor())
+}
+
+// パッケージ登録サボったのでinput関数を個別に定義
+func input(meg string) string {
+	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Print(meg + ": ")
+	scanner.Scan()
+	return scanner.Text()
+}
